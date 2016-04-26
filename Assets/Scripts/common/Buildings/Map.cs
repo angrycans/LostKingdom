eb76e73ca76c3a7dc7f9f13c53a1783c6ef5f0ac
Acans <acans@AcansiMac.local>
2016-04-26 17:53:53 +0800

    add map

---
+++ Assets/Scripts/common/Buildings/Map.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+//http://jsfiddle.net/bigbadwaffle/YeazH/
+namespace  Acans
+{
+
+	public class Room
+	{
+		public Room()
+		{
+		}
+
+	
+	}
+
+	public class Map
+	{
+
+		Room rooms;
+		public Map()
+		{
+		}
+
+		public Boolean DoesCollide(Room room, int ignore)
+		{
+			for (var i = 0; i < this.rooms.length; i++) {
+				if (i == ignore) continue;
+				var check = rooms[i];
+				if (!((room.x + room.w < check.x) || (room.x > check.x + check.w) || (room.y + room.h < check.y) || (room.y > check.y + check.h))) return true;
+			}
+
+			return false;
+		}
+
+
+
+	}
+
+
+
+}
+
---
+++ Assets/Scripts/common/test/GenerateMap.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace Acans
+{
+	public class GenerateMap
+	{
+		public GenerateMap()
+		{
+		}
+	}
+}
+
